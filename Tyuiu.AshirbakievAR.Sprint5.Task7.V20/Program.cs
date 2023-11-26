24434ead3cb7dd97109cf96f503dfbf29185c951
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-11-26 18:40:19 +0500

    Task7

---
+++ Tyuiu.AshirbakievAR.Sprint5.Task7.V20/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.AshirbakievAR.Sprint5.Task7.V20.Lib;
+
+namespace Tyuiu.AshirbakievAR.Sprint5.Task7.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Аширбакиев А.Р. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Аширбакиев Алмаз Ринатович | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask7V7.txt в котором есть набор   *");
+            Console.WriteLine("* символьных данных.Заменить все удвоенные буквы \"сс\" на \"с\".             *");
+            Console.WriteLine("* Полученный результат сохранить в файл OutPutDataFileTask7V20.txt.       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask7V20.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Находится в файле: ");
+            Console.WriteLine(ds.LoadDataAndSave(path));
+            Console.ReadKey();
+        }
+    }
+}
