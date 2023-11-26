0b1dd6ea8d4ebc598845bc3478b1d6a9b8a985ce
Tyuiu.AshirbakievAR@outlook.com <almazashirbakiev@gmail.com>
2023-11-26 15:38:44 +0500

    Task6

---
+++ Tyuiu.AshirbakievAR.Sprint5.Task6.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AshirbakievAR.Sprint5.Task6.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AshirbakievAR.Sprint5.Task6.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d58cbab-a536-4383-91ef-a9ceee6de356")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
